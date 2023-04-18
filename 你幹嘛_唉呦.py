19af940626fc12cb792b8e9715efc3349cac4e88
Hermit <128883101+Hermit1003@users.noreply.github.com>
2023-04-18 19:43:37 +0800

    Update 你幹嘛_唉呦.py

--- 你幹嘛_唉呦.py
+++ 你幹嘛_唉呦.py
@@ -4,6 +4,8 @@ import aiohttp
 from bs4 import BeautifulSoup
 import discord
 import constants
+import requests
+import json
 
 intents = discord.Intents().all()
 intents.message_content = True
@@ -18,21 +20,36 @@ UPDATE_INTERVAL = 3600  # 每小時更新一次
 client = discord.Client(intents=intents)
 
 #讀取json，輸出command list
+async def command_list(message):
+    # 指定 JSON 檔案的網址
+    json_url = "https://raw.githubusercontent.com/Hermit1003/tnfshbot/main/command_list.json"
 
-async def command_list():
-     return
+    # 發送 GET 請求取得 JSON 檔案的內容
+    response = requests.get(json_url)
+
+    # 檢查請求是否成功(200成功)
+    if response.status_code == 200:
+        # 使用 json.loads() 函數解析 JSON 檔案的內容為 Python 字典物件
+        command_list = json.loads(response.text)
+        # 現在 command_list 變數中就包含了 JSON 檔案的內容，可以直接使用
+        await message.channel.send(f"> **help**:{command_list['help']}\n> **command**:{command_list['command']}\n> **news**:{command_list['news']}\n{command_list['tip']}")
+    else:
+        print("無法取得 JSON 檔案，錯誤碼:", response.status_code)
 
 #DM用戶公告詳情
-async def show_details(url: str, interaction: discord.Interaction):
+async def show_details(url: str, title: str, interaction: discord.Interaction):
     user = interaction.user
     try:
-        await user.send(f"已訂選公告: {url}")
+        await user.send(f"已釘選公告: {url}")
+        if "置頂" in title:
+            title = title[:2] + title[4:]
+        await interaction.response.send_message(f"*已釘選消息({title})，請至您的私人訊息查看。*", ephemeral=True)
     except discord.Forbidden:
         # 如果用戶關閉了私人訊息，將無法發送訊息
-        await interaction.response.send_message("很遺憾的，您的私訊功能已關閉，我無法傳送訊息給您TAT")
+        await interaction.response.send_message("*由於您關閉了私人訊息，我無法傳送訊息給您，請開啟後再試試吧！*", ephemeral=True)
     except Exception as e:
         # 其他異常情況
-        await interaction.response.send_message(f"錯誤發生，請回報錯誤代碼給相關人員: {str(e)}")
+        await interaction.response.send_message(f"*發生錯誤，請回報錯誤資訊給相關人員: {str(e)}*", ephemeral=True)
 
 
 # 定義更新公告的函式
@@ -60,9 +77,6 @@ async def update_announcement(f: int, l: int):
     print('Getting Discord channel...')
     channel = client.get_channel(constants.DISCORD_CHANNEL_ID)
 
-    print('Getting UserID...')
-
-
     # 發送訊息到指定聊天頻道
     print('Sending message to Discord channel...')
 
@@ -81,12 +95,13 @@ async def update_announcement(f: int, l: int):
             message = '\n'.join(message_lines)
             if "置頂" in message:
                 message = f">>> {message[:2]}[{message[2:4]}]{message[4:]}"
+                title = message_lines[0]
             view = discord.ui.View()
             button_go_to_announcement = discord.ui.Button(label="前往公告", url=f"{url}")
             view.add_item(button_go_to_announcement)
 
             button_show_details = discord.ui.Button(label="釘選至私人訊息", style=discord.ButtonStyle.primary)
-            button_show_details.callback = partial(show_details, url)
+            button_show_details.callback = partial(show_details, url, title)
 
             view.add_item(button_show_details)
 
@@ -113,12 +128,12 @@ async def on_message(message):
     if message.content.startswith("t!"):
         usermsg = message.content.split("!")
         if usermsg[1] == "help":
-            await message.channel.send("Please send `t!command` to check command list.")
+            await message.channel.send("發送`t!command`來獲取指令列表")
         if usermsg[1] == "news":
             print('Updating announcement...')
             await update_announcement(1, 6)
         if usermsg[1] == "command":
-            await command_list()#還在做，目前想讓他輸出線上json，這樣就不用塞文字進來了
+            await command_list(message)#還在做，目前想讓他輸出線上json，這樣就不用塞文字進來了
 
 
 # 啟動 Discord bot 客戶端
